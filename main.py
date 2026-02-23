61420de65ffe36084206c3d062e35f9853d9ba03
dhruv <dhruvlgd44@gmail.com>
2026-02-23 18:55:20 +0530

    modified structure

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #hi
+import os 
+import requests
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+openai_client = OpenAI(api_keys = os.getenv("First_Key"))
+
+GROK_API_KEY = os.getenv("Second_Key")
